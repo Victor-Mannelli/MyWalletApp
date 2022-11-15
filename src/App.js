9e727d15faa3dfd4a632c3b9b558f003f0dddf8c
Victor-Mannelli <imp.mannelli@gmail.com>
2022-11-15 14:54:15 -0300

    feat: layout login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import GlobalStyle from "./GlobalStyle"
+import Login from "./Routs/Login"
 
 export default function App() {
 	return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+      <GlobalStyle />
+    </BrowserRouter>
   )
 }
---
+++ src/Routs/Login.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import styled from "styled-components";
+import axios from "axios"
+
+export default function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function HandleSubmit(){
+        axios.get()
+    }
+
+	return (
+		<LoginPage>
+			<h1> My Wallet </h1>
+			<LoginForm onSubmit={HandleSubmit}>
+				<input 
+                    required 
+                    placeholder="Email" 
+                    name="email"
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+				<input 
+                    required 
+                    placeholder="Password" 
+                    name="password"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+				<button type="submit"> Log in </button>
+			</LoginForm>
+			<p>First Time? Register now!</p>
+		</LoginPage>
+	);
+}
+
+const LoginPage = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	width: 100%;
+	height: 100vh;
+	background-color: var(--darkmode);
+	color: white;
+    font-size: 20px;
+    h1 {
+        font-size: 30px;
+        padding: 15px 0;
+    }
+    p {
+        font-size: 20px;
+        margin: 15px 0;
+        cursor: pointer;
+    }
+`;
+const LoginForm = styled.form`
+	display: flex;
+	flex-direction: column;
+	input {
+		width: 326px;
+		height: 58px;
+		border-radius: 5px;
+		background-color: lightgray;
+		margin: 5px 0;
+        padding: 10px;
+        font-size: 20px;
+        ::placeholder {
+            font-size: 20px;
+        }
+	}
+	button {
+		width: 326px;
+		height: 58px;
+		border-radius: 5px;
+		background-color: gray;
+		margin: 5px 0;
+        font-size: 20px;
+        color: var(--darkmode);
+        :hover {
+            background-color: lightgray;
+        }
+	}
+`;
